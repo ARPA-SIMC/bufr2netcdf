1603bd4082b690a78c1bf23fb0648e176aa12358
Enrico Zini <enrico@enricozini.org>
2011-06-01 13:51:52 +0000

    Command line parsing and first functional converter
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/bufr2netcdf/trunk@3235 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- src/bufr2netcdf.cc
+++ src/bufr2netcdf.cc
@@ -1,16 +1,79 @@
 #include "convert.h"
+#include <wreport/error.h>
+#include <string>
 #include <cstdio>
+#include <getopt.h>
 
 using namespace b2nc;
+using namespace wreport;
+using namespace std;
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-    Outfile outfile(argv[1]);
+    static struct option long_options[] =
+    {
+        /* These options set a flag. */
+        {"outfile", required_argument, NULL, 'o'},
+        {"verbose", no_argument,       NULL, 'v'},
+        {0, 0, 0, 0}
+    };
 
-    for (int i = 2; i < argc; ++i)
-        outfile.add_bufr(argv[i]);
+    string outfilename;
+    bool verbose = false;
 
-    outfile.close();
+    while (1)
+    {
+        /* `getopt_long' stores the option index here. */
+        int option_index = 0;
+
+        int c = getopt_long(argc, argv, "o:v",
+                long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+            case 'o':
+                outfilename = optarg;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            default:
+                error_consistency::throwf("unknown option character %c (%d)", c, c);
+                break;
+        }
+    }
+
+    if (optind >= argc)
+    {
+        fprintf(stderr, "Usage: %s [-o file] file1 [file2 [file3 ..]]\n", argv[0]);
+        return 1;
+    }
+
+    if (outfilename.empty())
+    {
+        outfilename = argv[optind];
+        outfilename += ".nc";
+    }
+
+    try {
+        if (verbose) fprintf(stderr, "Writing to %s\n", outfilename.c_str());
+        Outfile outfile(outfilename);
+
+        while (optind < argc)
+        {
+            if (verbose) fprintf(stderr, "Reading from %s\n", argv[optind]);
+            outfile.add_bufr(argv[optind++]);
+        }
+
+        outfile.close();
+    } catch (std::exception& e) {
+        fprintf(stderr, "%s\n", e.what());
+        return 1;
+    }
 
     return 0;
 }
